d821a89e780910841edcffe7471051b49ea40c8b
haroldohenrique <maggot36266@gmail.com>
2025-07-02 23:32:58 -0300

    add profile candidate and get method

---
+++ src/main/java/br/com/haroldohenriquedasneves/gestao_vagas/modules/candidate/useCases/ProfileCandidateUseCase.java
@@ -0,0 +1,31 @@
+package br.com.haroldohenriquedasneves.gestao_vagas.modules.candidate.useCases;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import br.com.haroldohenriquedasneves.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.haroldohenriquedasneves.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
+
+public class ProfileCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public ProfileCandidateResponseDTO execute(UUID candidateId) {
+        var candidate = this.candidateRepository.findById(candidateId)
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("User not found");
+                });
+        var candidateDTO = ProfileCandidateResponseDTO.builder()
+                .id(candidate.getId())
+                .name(candidate.getName())
+                .email(candidate.getEmail())
+                .username(candidate.getUsername())
+                .description(candidate.getDescription())
+                .build();
+
+        return candidateDTO;
+    }
+}
